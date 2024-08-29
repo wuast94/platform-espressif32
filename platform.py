b25578dab1ded5341361b6efbc99e767d98250b2
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-29 21:29:44 +0200

    Use espressif Arduino branch release/v3.1.x

--- platform.py
+++ platform.py
@@ -38,7 +38,7 @@ class Espressif32Platform(PlatformBase):
             self.packages["framework-arduinoespressif32"]["optional"] = False
             self.packages["framework-arduinoespressif32-libs"]["optional"] = False
             # use latest espressif Arduino libs
-            URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/idf-release/v5.3/package/package_esp32_index.template.json"
+            URL = "https://raw.githubusercontent.com/espressif/arduino-esp32/release/v3.1.x/package/package_esp32_index.template.json"
             packjdata = requests.get(URL).json()
             dyn_lib_url = packjdata['packages'][0]['tools'][0]['systems'][0]['url']
             self.packages["framework-arduinoespressif32-libs"]["version"] = dyn_lib_url
